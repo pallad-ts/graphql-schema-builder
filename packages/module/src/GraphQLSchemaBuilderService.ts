4fe74a8446e872bc7364da7b349c2d2d2aaf094d
Łukasz Kużyński <lukasz.kuzynski@gmail.com>
2022-03-21 22:21:02 +0100

    Updated deps

--- packages/module/src/GraphQLSchemaBuilderService.ts
+++ packages/module/src/GraphQLSchemaBuilderService.ts
@@ -1,9 +1,10 @@
 import {Service, ServiceName} from "alpha-dic";
 import {GraphQLSchemaBuilder} from "./GraphQLSchemaBuilder";
 
+// eslint-disable-next-line @typescript-eslint/naming-convention
 export function GraphQLSchemaBuilderService(opts?: { order?: number, name?: ServiceName }) {
-    return function (clazz: new(...args: any[]) => any) {
-        Service(opts?.name)(clazz);
-        GraphQLSchemaBuilder(opts?.order)(clazz);
-    }
+	return function (clazz: new(...args: any[]) => any) {
+		Service(opts?.name)(clazz);
+		GraphQLSchemaBuilder(opts?.order)(clazz);
+	}
 }
--- packages/module/src/InjectGraphqlSchema.ts
+++ packages/module/src/InjectGraphqlSchema.ts
@@ -1,6 +1,7 @@
 import {Inject, reference} from "alpha-dic";
 import {References} from "./References";
 
+// eslint-disable-next-line @typescript-eslint/naming-convention
 export function InjectGraphqlSchema() {
-    return Inject(reference(References.SCHEMA));
+	return Inject(reference(References.SCHEMA));
 }
